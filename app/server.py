480663c9e8eb844a62fbb81aa0e0ef859ea57ec1
Pierre Damiba <ptdamiba@gmail.com>
2020-01-06 18:13:49 -0500

    Replace server file

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/vmum5j7vjlo8r4j/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['one', 'two', 'three']
 path = Path(__file__).parent
 
 app = Starlette()
